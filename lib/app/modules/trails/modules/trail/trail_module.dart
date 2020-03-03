8d57ea0dbca1892db36fb7e682bc001942dc9b6d
Guilherme <guilhermevendra@gmail.com>
2020-03-03 20:24:06 -0300

    Refac pages

--- lib/app/modules/trails/modules/trail/trail_module.dart
+++ lib/app/modules/trails/modules/trail/trail_module.dart
@@ -1,6 +1,7 @@
 import 'package:ems/app/modules/trails/modules/trail/trail_controller.dart';
 import 'package:ems/app/modules/trails/modules/trail/trail_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:i18n_extension/i18n_widget.dart';
 
 class TrailModule extends ChildModule {
   @override
@@ -12,9 +13,11 @@ class TrailModule extends ChildModule {
   List<Router> get routers => [
         Router(
           '/:id/:trailId',
-          child: (_, args) => TrailPage(
-            id: int.tryParse(args.params['id']),
-            trailId: int.tryParse(args.params['trailId']),
+          child: (_, args) => I18n(
+            child: TrailPage(
+              id: int.tryParse(args.params['id']),
+              trailId: int.tryParse(args.params['trailId']),
+            ),
           ),
         ),
       ];
